56244e0b88f967e7fdd87bcf70be8fa6a30cee9c
selene <selene@gmail.com>
2024-02-08 20:43:18 -0500

    created React App

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import './styles/Home.css'
+
+
+function Home() {
+    return (
+        <div className="home-div">
+            <section className="main">
+                <h1>Where Homes find Their Stories</h1>
+                <p className="homeP">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, error
+                maxime debitis veniam dolor, assumenda hic vitae id nihil perferendis
+                facilis! Sed autem quas ipsam, eos optio natus dolorem corporis.
+                </p>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
